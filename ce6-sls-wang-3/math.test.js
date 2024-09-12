6b4a198abe050656dfcde48c5780a3b624b94f36
Wang Xin <xin_wang_xin@hotmail.com>
2024-09-12 21:04:44 +0800

    3rd commit handler

--- ce6-sls-wang-3/math.test.js
+++ ce6-sls-wang-3/math.test.js
@@ -1,4 +1,4 @@
-const { sum, substract, multiply } = require('./sum');
+const { sum, substract, multiply } = require('./math');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
